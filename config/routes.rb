1c9c7ad4ac679d9ac23f1778f2b5b7f4be16238c
Jason Herbert <jwsaus@gmail.com>
2016-09-16 11:45:28 +1000

    navbar and devise links

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  root 'pages#feed'
+  get 'pages/feed'
+
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
